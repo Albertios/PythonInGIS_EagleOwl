dba3871c9d26c7b23a70853ba01c29174b4a470b
Albert <a_hamz01@wwu.de>
2019-07-13 16:52:58 +0200

    create an array from shape file

--- Scripts/extractDataFromShapeFile.py
+++ Scripts/extractDataFromShapeFile.py
@@ -1,18 +1,38 @@
-#load poits.shp file 
+##################create a filtered CSV file#######################
+##load poits.shp file 
+#layer = iface.activeLayer()
+#
+## output file direction
+#dir = "/Users/Alf/Documents/GitHub/PythonInGIS_EagleOwl/"
+#
+##output file name
+#output_file = open(dir + "filteredOwlData.csv", 'w')
+#
+#
+#for f in layer.getFeatures():
+#    geom = f.geometry()
+#    line = '%s, %s, %f, %f\n' % (f['tag_ident'], f['timestamp'],
+#        geom.asPoint().y(), geom.asPoint().x())
+#    unicode_line = line.encode('utf-8')
+#    output_file.write(unicode_line.decode("utf-8") )
+#    
+#output_file.close()
+
+#_________________________________________________________________
+
+
+##################create a filtered array#######################
+
+#def filteredOwlData(layer):
+    
 layer = iface.activeLayer()
-
-# output file direction
-dir = "/Users/Alf/Documents/GitHub/PythonInGIS_EagleOwl/"
-
-#output file name
-output_file = open(dir + "filteredOwlData.csv", 'w')
-
-
+    
+owlIdTimestampXY = []
+    
 for f in layer.getFeatures():
     geom = f.geometry()
-    line = '%s, %s, %f, %f\n' % (f['tag_ident'], f['timestamp'],
-        geom.asPoint().y(), geom.asPoint().x())
-    unicode_line = line.encode('utf-8')
-    output_file.write(unicode_line.decode("utf-8") )
+    line = int(f['tag_ident']), f['timestamp'], geom.asPoint().y(), geom.asPoint().x()
+    owlIdTimestampXY.append(line)
     
-output_file.close()
+#return(owlIdTimestampXY)
+    
