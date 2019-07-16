a2598ca34a10949a3ad2f32cb098e42aa38fb9a6
rkans02 <rkans02@users.noreply.github.com>
2019-07-17 00:00:59 +0200

    update main

--- Scripts/main.py
+++ Scripts/main.py
@@ -11,8 +11,8 @@ os.system("python distances.py")
 os.system("python keepDataOverTenDays.py")
 
 # Working directory
-#data_dir = os.path.join('D:\\', 'Repositories', 'PythonInGIS_EagleOwl', 'eagle_owl')
-data_dir = os.path.join('/Users/Alf/Documents/GitHub', 'PythonInGIS_EagleOwl', 'eagle_owl')
+data_dir = os.path.join('D:\\', 'Repositories', 'PythonInGIS_EagleOwl', 'eagle_owl')
+#data_dir = os.path.join('/Users/Alf/Documents/GitHub', 'PythonInGIS_EagleOwl', 'eagle_owl')
 
 def init():
     QgsProject.instance().removeAllMapLayers()
@@ -35,42 +35,27 @@ def init():
     else: print("Shapefile loaded!") 
     
 
+
 def main():
     
-    #init()
+    init()
     
     layer = iface.activeLayer()
     
     fullTable = filteredOwlData(layer)
 
-    print(len(fullTable))
-    
-
     minimizedTable = keepDataOverTenDays(fullTable)
-    
-    # output file direction
-    dir = "/Users/Alf/Documents/GitHub/PythonInGIS_EagleOwl/"
 
-    #output file name
-    output_file = open(dir + "minimizedTableUTF.csv", 'w')
-
-
-    for i,f in enumerate(minimizedTable):
-
-        line = str('%s, %s, %f, %f\n' % f)
-        unicode_line = line.encode('utf-8')
-        #unicode_line = line
-        #output_file.write(str(unicode_line))
-        output_file.write(unicode_line.decode("utf-8") )
-    
-    output_file.close()
-
-    print(len(minimizedTable))
-    
+    global distanceTable    
     distanceTable = getDistances(minimizedTable)
-
+    
+    global monthTable
+    monthTable = computeMonthTable(distanceTable)
 
     
+    
+distanceTable = []
+monthTable = []
 main()
 
 
--- Scripts/monthDistance.py
+++ Scripts/monthDistance.py
@@ -7,16 +7,31 @@ import numpy as np
 def computeMonthTable(input):
     
     result = []
+    curDistance = 0
     
     for f in input:
-        timestamp = f[1]
-        #print(timestamp)
-        break
-    
-    
-    
+        curID = str(f[0])
+        timestamp = str(f[1])
+        year = timestamp[0:4]
+        month = timestamp[5:7]
+        distance = f[4]
+        
+        
+        curDistance = curDistance + distance
+        
+        
+        if(curID != "1751"):
+            break
     
     
     return result
     
+
+table = computeMonthTable(distanceTable)
+print(table[0])
+
+
+
+
+
     
